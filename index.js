e445602d89f1b0dc7886ac367b38210fbdf9cde3
manavm1990 <manavm@visionwebsoft.com>
2019-11-07 21:11:40 -0600

    Add for loop solution for 1 to 10 summing.

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 // Add the numbers from 1 to 10
 
-/**
- * Create a loop that starts at 1 and ends at 10.
- * Keep track of the current count.
- * Keep track of the 🏃🏾‍♂️ total.
- */
-
-let currentNum = 0;
 let currentTotal = 0;
 
-while(currentNum <= 10) {
-  currentTotal += currentNum;
 
-  currentNum += 1; // currentNum = currentNum + 1;
+// Initializer; Exit Condition; Iterator
+for(let i = 1; i <= 10; i += 1) {
+  console.log('i is', i);
+  currentTotal += i; // currentTotal = currentTotal + i;
+  console.log('currentTotal is', currentTotal);
 }
 
 console.log(currentTotal);
