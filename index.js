5a22e4e11c76eecab0b63299807c4fa3e4bf9627
manavm1990 <manavm@visionwebsoft.com>
2019-11-07 21:20:51 -0600

    Add default values for named parameters.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Add the numbers from 1 to 10
 
 // NAMED PARAMETERS allow FUNCTIONS to receive additional information to complete a task.
-function numberSummer(startingValue, endingValue) {
+// We can set DEFAULT VALUES for our NAMED PARAMETERS
+function numberSummer(startingValue = 1, endingValue = 10) {
   let currentTotal = 0;
 
   // Initializer; Exit Condition; Iterator
@@ -13,4 +14,4 @@ function numberSummer(startingValue, endingValue) {
 }
 
 // We INVOKE FUNCTIONS and can pass in ARGUMENTS that 'match up' to the NAMED PARAMETERS
-console.log(numberSummer(1, 10));
+console.log(numberSummer());
