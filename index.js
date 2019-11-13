3d4febef3e8156922d3debd40cc2b06cf8f5baaf
manavm1990 <manavm@visionwebsoft.com>
2019-11-12 20:37:14 -0600

    :construction: Add examples for function factories and closures.

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-// doMath is expecting to receive a function!
-function doMath(x, y, operation) {
-  console.trace();
-  // the function will receive x and y
-  return operation(x, y);
+/**
+ * This is a FUNCTION FACTORY that returns another function.
+ * This FUNCTION FACTORY uses the concept of a CLOSURE.
+ *
+ * initSportsScorer is the OUTER FUNCTION.
+ * It receives pts, but then immediately discards its reference to pts.
+ * Pts remains reference by the INNER FUNCTION - this is example of CLOSURE.
+ * A variable loses reference from the original function but
+ * has become 'enclosed' with a reference to the INNER FUNCTION.
+ */
+function initSportsScorer(pts) {
+  return function(score) {
+    return score + pts;
+  };
 }
 
-function subtract(x, y) {
-  console.trace();
+const threePointers = initSportsScorer(3);
+console.log(threePointers(99));
+/**
+ * function (score) {
+ *  return score + 3;
+ * }
+ */
 
-  return y - x;
-}
+const touchdown = initSportsScorer(6);
+console.log(touchdown(14));
+/**
+ * function (score) {
+ *  return score + 7;
+ * }
+ */
 
-// function add(x, y) {
-//   console.trace();
-
-//   return y + x;
-// }
-
-console.log(doMath(10, 5, subtract));
-// console.log(doMath(10, 5, add));
+const xtraPt = initSportsScorer(1);
+console.log(xtraPt(13));
