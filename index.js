3cfa9534851c7d6749825dd596c9f777bbdbbe70
manavm1990 <manavm@visionwebsoft.com>
2019-11-15 06:33:34 -0600

    Add FizzBuzz girections.

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-// Pass by reference and pass by value
-
-let x = 3;
-
-// y references its own COPY of the number 3
-let y = x;
-
-function add1(n) {
-  return n + 1;
-}
-
-// Primitives CANNOT be mutated.
-// Primitives are passed by value AKA pass by copy;
-
-// x THROWS AWAY it's '3' and gets a whole new value of 4;
-x = add1(x);
-console.log("x after add1 is", x);
-console.log(y);
+// Fizz Buzz
+// If we pass a number that's divisible by both 5 and 3, log 'fizzbuzz'
+// If it's only divisible by 3, log 'fizz'
+// If it's only divisible by 5, log 'buzz'
+// If it's not divisible by either 5 or 3 log the number.
+// Loop over the numbers from 1 to 99.
