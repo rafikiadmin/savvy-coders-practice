c7be61de0c53fe4430fd9c30e4edf9ffbf172387
manavm1990 <manavm@visionwebsoft.com>
2019-11-14 19:20:55 -0600

    Add for loop solution for psycho book.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function initGreeting(name, greeting) {
-  return function(time) {
-    return `${name} ${greeting}. The time is: ${time}`;
-  };
+// Use your looping skills to write out 'All work and no play makes Jack a dull boy' a given number of times.
+// Your function writePsychoBook(num)
+// Preferably, your function DOES NOT use 'console.log()
+// Instead, it builds (concatenates) a long string and returns it.
+// Then we can log it ourselves in the global scope.
+
+function writePsychoBook(num) {
+  let str = "";
+
+  for (let i = 1; i <= num; i += 1) {
+    str += "All work and no play makes Jack a dull boy! ";
+  }
+
+  return str;
 }
-
-const eveningGeorge = initGreeting("Geroge", "Evening");
-
-const time = new Date(Date.now());
-
-console.log(eveningGeorge(`${time.getHours()}:${time.getMinutes()}`));
