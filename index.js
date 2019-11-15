453275541f3b14e451d3ee05637570bda70eab35
manavm1990 <manavm@visionwebsoft.com>
2019-11-14 20:40:45 -0600

    Add recursive solution for factorial.

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-// Factorial - num * num - 1 * num - 2...all the way to num === 0
-// 4 * 3 * 2 * 1;
-
 function factorial(num) {
-  let ret = 1;
-
-  for (let i = num; i >= 1; i -= 1) {
-    console.log("ret is", ret);
-    console.log("i is", i);
-
-    ret *= i;
+  if (num === 0) {
+    return 1;
   }
 
-  return ret;
+  return num * factorial(num - 1);
 }
 
 console.log(factorial(4));
