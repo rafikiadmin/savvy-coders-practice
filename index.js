d0ddae08dff7dcf027f893b799689a379c1dc78d
manavm1990 <manavm@visionwebsoft.com>
2019-11-14 21:00:54 -0600

    Add examples of pass by value.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function factorial(num) {
-  if (num === 0) {
-    return 1;
-  }
+// Pass by reference and pass by value
 
-  return num * factorial(num - 1);
+let x = 3;
+
+// y references its own COPY of the number 3
+let y = x;
+
+function add1(n) {
+  return n + 1;
 }
 
-console.log(factorial(4));
+// Primitives CANNOT be mutated.
+// Primitives are passed by value AKA pass by copy;
+
+// x THROWS AWAY it's '3' and gets a whole new value of 4;
+x = add1(x);
+console.log("x after add1 is", x);
+console.log(y);
