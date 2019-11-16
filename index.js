c77d4ca6732ea27752ae5451300f54c757a6d160
manavm1990 <manavm@visionwebsoft.com>
2019-11-15 19:13:10 -0600

    Add comments about scoping.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function logNumbers(num) {
   let count = 1;
 
+  // while requires access to a variable outside of its scope. 👎🏾
   while (count <= num) {
+    // Scope is opened every time there is a brace
     console.log(count);
     count += 1;
   }
