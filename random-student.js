fbe816cc9e5572cb37500c3fe6282ab2d487e2e4
manavm1990 <manavm@visionwebsoft.com>
2019-11-15 21:29:00 -0600

    Add example of looping in Array to get names.

--- random-student.js
+++ random-student.js
@@ -14,6 +14,18 @@ const students = [
   "Wallat"
 ];
 
+function getMs(namesArr) {
+  const mNames = [];
+
+  for (let i = 0; i < namesArr.length; i += 1) {
+    if (namesArr[i].startsWith("M")) {
+      mNames.push(namesArr[i]);
+    }
+  }
+
+  return mNames;
+}
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -22,3 +34,5 @@ function getRandomInt(min, max) {
 }
 
 console.log(students[getRandomInt(0, students.length - 1)]);
+
+console.log(getMs(students));
