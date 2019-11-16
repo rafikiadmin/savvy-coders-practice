53c4a029fe22948ee7d3698401f6b00a2ccb906f
manavm1990 <manavm@visionwebsoft.com>
2019-11-15 18:41:29 -0600

    Add example of function factory for OOP.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// Fizz Buzz
-// If we pass a number that's divisible by both 5 and 3, log 'fizzbuzz'
-// If it's only divisible by 3, log 'fizz'
-// If it's only divisible by 5, log 'buzz'
-// If it's not divisible by either 5 or 3 log the number.
-// Loop over the numbers from 1 to 99.
+function initPerson(fname, lname, age) {
+  return {
+    firstName: fname,
+    lastName: lname,
+    age: 23,
+    canDrink: function() {
+      return age >= 21;
+    }
+  };
+}
+
+const me = initPerson("manav", "misra", 13);
+
+console.log(me.canDrink());
