2c50a1e417519076c6d3d06d1e08df365873b5e8
manavm1990 <manavm@visionwebsoft.com>
2019-11-15 19:16:43 -0600

    Add example of simple 'bad' function with for loop.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function logNumbers(num) {
-  let count = 1;
-
-  // while requires access to a variable outside of its scope. 👎🏾
-  while (count <= num) {
-    // Scope is opened every time there is a brace
-    console.log(count);
-    count += 1;
+  for (let i = 1; i <= num; i += 1) {
+    /**
+     * Developer's Note: This is a bad, impure function that
+     * reaches out to the global the window instead of returning.
+     */
+    console.log(i);
   }
 }
 
