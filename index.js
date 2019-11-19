45bcb2f056884006841ea37dd94e5b86759af8b9
manavm1990 <manavm@visionwebsoft.com>
2019-11-18 19:07:45 -0600

    Add alternate solution to check for divisible by both numbers.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-function fizzBuzz() {
-  for (let i = 1; i <= 100; i += 1) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
+function initFizzBuzz(num1, num2) {
+  return function() {
+    for (let i = 1; i <= 100; i += 1) {
+      // 24 % (4 * 6)
+      // If i divided by num1 * num2 is 0, then then number is divisible by both.
+      if (i % (num1 * num2) === 0) {
+        console.log(`${i} - FizzBuzz`);
+      } else if (i % num1 === 0) {
+        console.log(`${i} - Fizz`);
+      } else if (i % num2 === 0) {
+        console.log(`${i} - Buzz`);
+      } else {
+        console.log(i);
+      }
     }
-  }
+  };
 }
 
-fizzBuzz();
+const fizzBuzz46 = initFizzBuzz(4, 6);
+fizzBuzz46();
