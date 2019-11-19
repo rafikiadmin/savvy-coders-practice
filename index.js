b535622e2228ea4b962b10726136b2c49eec17e4
manavm1990 <manavm@visionwebsoft.com>
2019-11-18 19:40:52 -0600

    Add alternate solution for FizzBuzz using unary operator not.

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ function initFizzBuzz(num1, num2) {
       // If i divided by num1 * num2 is 0, then then number is divisible by both.
       if (i % (num1 * num2) === 0) {
         console.log(`${i} - FizzBuzz`);
-      } else if (i % num1 === 0) {
-        console.log(`${i} - Fizz`);
-      } else if (i % num2 === 0) {
+
+        // ! means not
+      } else if (!(i % num1)) {
+        console.log(`${i}  Fizz`);
+
+        // Else-if there is not any remainder (i % num2 === 0)
+      } else if (!(i % num2)) {
         console.log(`${i} - Buzz`);
       } else {
         console.log(i);
@@ -15,6 +19,3 @@ function initFizzBuzz(num1, num2) {
     }
   };
 }
-
-const fizzBuzz46 = initFizzBuzz(4, 6);
-fizzBuzz46();
