25a829aa0e1341f68692bbf4d7cf4336d5db1297
manavm1990 <manavm@visionwebsoft.com>
2019-11-19 20:10:33 -0600

    Add example of setter.

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-// Function constructors by convention are capitalized.
-function Person(fname, lname, age) {
-  /**
-   * Although we should generally stick to including `this` inside function constructors,
-   * note that one example of matching 🔑 names with the NAMED PARAMETERS,
-   * is that as long as nothing changes,
-   * we can omit 'this' and we can use ARROW SYNTAX.
-   *
-   * Again, this could be a bit brittle if there are any minor changes and I would stick with using `this` and keyword function, en generalmente.
-   */
-  this.fname = fname;
-  this.lname = lname;
-  this.age = age;
+function Food(breakfast, lunch, dinner) {
+  this.breakfast = breakfast;
+  this.lunch = lunch;
+  this.dinner = dinner;
 
-  this.getFullName = () => {
-    return `${fname} ${lname}`;
+  this.breakfastDrink = function() {
+    if (this.breakfastDrink === "milk") {
+      return `I would like some ${breakfast}`;
+    }
+    return `I'm lactose intolerant.`;
   };
 
-  this.canVote = () => {
-    if (age >= 18) {
-      return "I vote!";
+  this.eatOut = function() {
+    if (this.lunch === "yes") {
+      return "I would love teat at restaurant";
     }
 
-    return "No voting for me!";
+    return "I will just cook";
+  };
+
+  this.setSnack = function(snack) {
+    // TODO: Check if snack is a non-empty string (use typeof)
+    if (!snack || typeof snack !== "string") {
+      this.snack = snack;
+    } else {
+      console.error("snack cannot be blank or a non-string");
+    }
   };
 }
 
-const older = new Person("joh", "smith", 77);
-
-console.log("older says", older);
-console.log(older.getFullName());
-console.log("older.canVote says", older.canVote());
+const foodPlan = new Food("milk", "yes", "pasta");
