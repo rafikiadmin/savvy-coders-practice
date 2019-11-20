358109233c6fd435b42c61225a5985c716a65582
manavm1990 <manavm@visionwebsoft.com>
2019-11-19 20:33:45 -0600

    Add prototype example with food.

--- index.js
+++ index.js
@@ -2,30 +2,40 @@ function Food(breakfast, lunch, dinner) {
   this.breakfast = breakfast;
   this.lunch = lunch;
   this.dinner = dinner;
-
-  this.breakfastDrink = function() {
-    if (this.breakfastDrink === "milk") {
-      return `I would like some ${breakfast}`;
-    }
-    return `I'm lactose intolerant.`;
-  };
-
-  this.eatOut = function() {
-    if (this.lunch === "yes") {
-      return "I would love teat at restaurant";
-    }
-
-    return "I will just cook";
-  };
-
-  this.setSnack = function(snack) {
-    // TODO: Check if snack is a non-empty string (use typeof)
-    if (!snack || typeof snack !== "string") {
-      this.snack = snack;
-    } else {
-      console.error("snack cannot be blank or a non-string");
-    }
-  };
 }
 
+Food.prototype.breakfastDrink = function() {
+  if (this.breakfastDrink === "milk") {
+    return `I would like some ${this.breakfast}`;
+  }
+  return `I'm lactose intolerant.`;
+};
+
+Food.prototype.eatOut = function() {
+  if (this.lunch === "yes") {
+    return "I would love teat at restaurant";
+  }
+
+  return "I will just cook";
+};
+
+Food.prototype.setSnack = function(snack) {
+  if (snack && typeof snack === "string") {
+    this.snack = snack;
+  } else {
+    console.error("snack cannot be blank or a non-string");
+  }
+};
+
 const foodPlan = new Food("milk", "yes", "pasta");
+const aPlan = new Food("milk", "yes", "pasta");
+
+const fPlan = new Food("milk", "yes", "pasta");
+
+console.log(foodPlan, aPlan, fPlan);
+
+console.log(Food.prototype);
+
+// console.log("before set snack", foodPlan);
+// foodPlan.setSnack("chips");
+// console.log("after set snack", foodPlan);
