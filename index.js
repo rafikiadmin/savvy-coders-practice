8496be1b3ad9c0566dcaa82ec4ca28e7ac420b34
Manav Misra <manavm@visionwebsoft.com>
2019-11-26 19:03:27 -0600

    Add example of array concatenation and iteration.

--- index.js
+++ index.js
@@ -1,86 +1,19 @@
-// CLASS DOES NOT CHANGE THE FACT THAT WE ARE DOING PROTOTYPE-BASED OOP AND NOT CLASS-BASED OOP! 📢
-// We will use class to implement function constructor prototypes.
-class Person {
-  constructor(fname, lname, age, occupation) {
-    this.fname = fname;
-    this.lname = lname;
-    this.age = age;
-    this.occupation = occupation;
-  }
+// Iterate over an ARRAY with WHILE and separate out the NUMBERS and STRINGS
+const numbers = [1, 2, 3, 4, 5, 6];
+const strings = ["hello", "world", "again"];
 
-  get bio() {
-    return `Hi! My name is ${this.fullName}. I am ${this.age} years old. ${this.drink}`;
-  }
+// concat is ideal b/c it doesn't mutate the original data.
+const numbersAndStrings = numbers.concat(strings);
 
-  get drink() {
-    if (this.age < 21) {
-      return "Give me a Shirley Temple!";
-    }
+const updatedNumbersAndStrings = [];
 
-    return "🙆🏾‍♂️, I will have a 🍺";
-  }
-
-  get fullName() {
-    return `${this.fname} ${this.lname}`;
-  }
-
-  // The name of the setter MUST BE DIFFERENT from the name of the property.
-  set changeName(newLName) {
-    if (newLName) {
-      this.lname = newLName;
-    } else {
-      console.error("bad name received");
-    }
-  }
-
-  // This causes a blown stack due to recursion.
-  // set lname(newLName) {
-  //   this.lname = newLName;
-  // }
-}
-
-// We are passing in arguments that the constructor 👆 will use to bind to an instance's properties.
-const p = new Person("joh", "smit", 33, "insurance agent");
-
-class Employee extends Person {
-  constructor(fname, lname, age, occupation, empid) {
-    // Sends some of the arguments are up to the 'supevisor' and assign these to THIS instance.
-    super(fname, lname, age, occupation);
-    this.id = empid;
-  }
-
-  get bio() {
-    return `I love my job. ${this.id}`;
-  }
-
-  get empid() {
-    return `My id is: ${this.id}`;
-  }
-
-  get assignments() {
-    let ret = `The current task list is as folows:`;
-
-    for (let i = 0; i < this.workload.length; i += 1) {
-      ret += `${this.workload[i]}`;
-    }
-
-    return ret;
-  }
-
-  set empid(id) {
-    this.id = id;
-  }
-
-  set assignments(assignments) {
-    this.workload = assignments;
+for (let i = 0; i < numbersAndStrings.length; i += 1) {
+  // If the element is a string concatenate "Hello, I am a string" to the end of it.
+  if (typeof numbersAndStrings[i] === "string") {
+    updatedNumbersAndStrings.push((numbersAndStrings[i] += "ehllo"));
+  } else {
+    updatedNumbersAndStrings.push((numbersAndStrings[i] += 236));
   }
 }
 
-const emp = new Employee("Mark", "west", 33, "dev", "123");
-
-emp.empid = 234;
-
-console.log(emp.bio);
-
-emp.assignments = ["REdord vidoes", "write curriculum", "grade HW"];
-console.log(emp.assignments);
+console.log(updatedNumbersAndStrings);
